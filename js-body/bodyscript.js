1b73d357311fc3a664b33e6c8033f379f0d7157d
fruiz500 <fruiz500@gmail.com>
2020-02-26 10:42:48 -0600

    version 0.1.5
    Updated third-party libraries

--- js-body/bodyscript.js
+++ js-body/bodyscript.js
@@ -48,7 +48,7 @@ function textheight(){
 				mainBox.style.height = fullheight - offsetheight + 'px';
 			}
 		}else{
-			mainBox.style.height = fullheight - offsetheight + 'px';
+			mainBox.style.height = fullheight - offsetheight - 10 + 'px';
 		}
 	}
 }
